e9c2bb0b626124cb62cb48a4ddb5af0a3b6620b1
arcadia168 <bennawaz168@icloud.com>
2019-06-14 08:47:23 +0100

    Configuring this app so that it can run in GCloud

--- dist/config.js
+++ dist/config.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var config = {
-    port: process.env.NODE_PORT || 3000,
+    port: process.env.PORT || 8080,
     prettyLog: process.env.NODE_ENV == 'development',
 };
 exports.config = config;
